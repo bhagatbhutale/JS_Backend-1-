208c76cf5a39901a24d6a1603d23b57dd7cd05f2
BhagatBhutale <bhagatbhutale57@gmail.com>
2025-02-01 16:43:20 +0530

    new path added in webpage

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -17,6 +17,10 @@ app.get("/login", (req, res) => {
 res.send(`<h1>Please login at website</h1>`)
 })
 
-app.listen(process.env.PORT, () => {
+app.get("/youtube", (req, res) => {
+    res.send("<h2?>this is a Youtube clone webpage</h2?")
+})
+
+app.listen(8080, () => {
   console.log(`Example app listening on port ${port}`);
 });
